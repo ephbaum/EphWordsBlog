edf756a8fcf5e8a88f06b6504fddd9880327c2ec
Eph Baum <eph_baum@outlook.com>
2023-10-19 21:29:35 -0700

    (fix) optimize new script
    (fix) return to UTC, seems local time doesn't jive

--- bin/newpost.sh
+++ bin/newpost.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Navigate to the directory containing the script
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Navigate to the directory containing the script using parameter expansion
+script_dir="$(dirname -- "${BASH_SOURCE[0]}")"
 
 # Get the current date in YYYY-MM-DD format
 current_date=$(date +"%Y-%m-%d")
 
 # Prompt for the suffix for the markdown file name
-read -p "Enter the suffix for the markdown file name: " name_suffix
+read -p "Enter the post title for the markdown file name: " name_suffix
 
 # Full markdown file name
 file_name="${current_date}-${name_suffix}.md"
@@ -16,23 +16,25 @@ file_name="${current_date}-${name_suffix}.md"
 dir="${script_dir}/../src/content/blog/"
 
 # Check if directory exists, create if not
-mkdir -p $dir
+mkdir -p "$dir"
 
-# Current datetime in ISO 8601 format, local time
-current_datetime=$(date +"%Y-%m-%dT%H:%M:%S.000%z")
+# Current datetime in ISO 8601 format
+current_datetime=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
 
 # Create the markdown file with default frontmatter
-echo "---" > "${dir}${file_name}"
-echo "title: New Post" >> "${dir}${file_name}"
-echo "postSlug: new-post" >> "${dir}${file_name}"
-echo "pubDatetime: ${current_datetime}" >> "${dir}${file_name}"
-echo "ogImage: ../../assets/images/2023" >> "${dir}${file_name}"
-echo "featured: false" >> "${dir}${file_name}"
-echo "draft: false" >> "${dir}${file_name}"
-echo "tags:" >> "${dir}${file_name}"
-echo "  - default" >> "${dir}${file_name}"
-echo "description: Placeholder description for new post" >> "${dir}${file_name}"
-echo "---" >> "${dir}${file_name}"
+cat <<EOL > "${dir}${file_name}"
+---
+title: New Post
+postSlug: new-post
+pubDatetime: ${current_datetime}
+ogImage: ../../assets/images/2023
+featured: false
+draft: false
+tags:
+  - default
+description: Placeholder description for new post
+---
+EOL
 
 # Confirm file creation
 echo "Markdown file ${file_name} has been created in ${dir}"
