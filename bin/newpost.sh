4f9a174539f7124f2a44e0461141ea01534d8329
Eph Baum <eph_baum@outlook.com>
2023-12-30 19:15:10 -0800

    make the new post script a little more robust nad dynamic because distraction

--- bin/newpost.sh
+++ bin/newpost.sh
@@ -6,17 +6,40 @@ script_dir="$(dirname -- "${BASH_SOURCE[0]}")"
 # Get the current date in YYYY-MM-DD format
 current_date=$(date +"%Y-%m-%d")
 
+# Get the current year in YYYY format
+year=$(date +"%Y")
+
+# Get the current month in MM format
+month=$(date +"%m")
+
+# Get the current day in DD format
+day=$(date +"%d")
+
 # Prompt for the suffix for the markdown file name
 read -p "Enter the post title for the markdown file name: " name_suffix
+
+title=${name_suffix}
+
+# Replace spaces with dashes
+name_suffix=${name_suffix// /-}
+
+# Replace uppercase with lowercase
+name_suffix=${name_suffix,,}
 
 # Full markdown file name
 file_name="${current_date}-${name_suffix}.md"
 
 # Directory to save the markdown file, relative to /bin
-dir="${script_dir}/../src/content/blog/"
+dir="${script_dir}/../src/content/blog/${year}/${month}/"
 
 # Check if directory exists, create if not
-mkdir -p "$dir"
+mkdir -p "${dir}"
+
+# create ogImage directory string
+ogImageDir="/assets/images/${year}/${month}/"
+
+# Create ogImage directory if it doesn't exist
+mkdir -p "${script_dir}/../static${ogImageDir}"
 
 # Current datetime in ISO 8601 format
 current_datetime=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
@@ -24,10 +47,10 @@ current_datetime=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
 # Create the markdown file with default frontmatter
 cat <<EOL > "${dir}${file_name}"
 ---
-title: New Post
+title: ${title}
 postSlug: new-post
 pubDatetime: ${current_datetime}
-ogImage: ../../assets/images/2023
+# ogImage: ${ogImageDir}
 featured: false
 draft: false
 tags:
